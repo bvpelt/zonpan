0aec8e09ac0bd632cff3c79ff07f41047733a40f
Bart van Pelt <bart.vanpelt@gmail.com>
2016-10-08 21:52:57 +0200

    First commit

---
+++ gapapp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gapappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gapappApp
+ */
+angular.module('gapappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ gapapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gapappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gapappApp
+ */
+angular.module('gapappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
