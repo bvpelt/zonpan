aa0814c39311bdbef28c2637ecbdd90cc3c37293
Bart van Pelt <bart.vanpelt@gmail.com>
2016-10-21 20:51:33 +0200

    First release

--- src/js/zonpan-service.js
+++ src/js/zonpan-service.js
@@ -1,26 +1,24 @@
 angular.module('myApp.services', [])
-	.factory('zonpanService', function($http) {
+	.factory('zonpanService', function ($http) {
 
 		var zonpanHost = 'https://monitoringapi.solaredge.com/';
 		var zonpanAPIKey = 'OFKMR38I1N2SDMD9I65V6PVKTD7VM7QZ';
-		//var githubUrl = 'https://api.github.com';
 		var zonpanOperation = 'sites/list';
 
-		var runRequest = function(operatie) {
+		var runRequest = function (operatie) {
 			// Return the promise from the $http service
 			// that calls the Github API using JSONP
 			return $http({
 				method: 'JSONP',
 				url: zonpanHost + operatie + '?api_key=' + zonpanAPIKey + '&callback=JSON_CALLBACK'
-				//url: zonpanHost + zonpanOperation + '?api_key=' + zonpanAPIKey + '&callback=JSON_CALLBACK'
 			});
 		}
 		// Return the service object with a single function
 		// events
 		return {
-			events: function(operatie) {
+			events: function (operatie) {
 				return runRequest(operatie);
-				}
+			}
 		};
 	}
-);
+	);
